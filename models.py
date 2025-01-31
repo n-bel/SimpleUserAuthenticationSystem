f5adc98f44097e675c749160b2ba93066e0d47d3
felfel <naoufalbelgada@gmail.com>
2025-01-31 05:29:15 +0100

    corrected error that made user unable to register

--- models.py
+++ models.py
@@ -11,6 +11,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
 
+
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
--- routes.py
+++ routes.py
@@ -12,7 +12,7 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User()
+        user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
